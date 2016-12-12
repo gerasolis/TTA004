27e164536d7298b4793bdaf0d711858e8b16cde2
gerasolis <gisolisgr@gmail.com>
2016-12-11 19:48:32 -0600

    Ya obtiene los errores del html. Sólo falta insertar en la bd

--- src/main/java/mx/prisma/generadorPruebas/bs/EjecutarPruebaBs.java
+++ src/main/java/mx/prisma/generadorPruebas/bs/EjecutarPruebaBs.java
@@ -4,8 +4,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import mx.prisma.editor.model.CasoUso;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 public class EjecutarPruebaBs {
 
@@ -29,8 +33,36 @@ public class EjecutarPruebaBs {
                 	System.out.println(line); 
                 	String lineaErrores = line.substring(line.indexOf("{"), line.indexOf("}]}"))+"}]}";
                 	System.out.println(lineaErrores);
-                	//Ahora convertirlo a json y trabajarlo como tal.
-                	//JSONObject json = (JSONObject) new JSONParser().parse(lineaErrores);
+                	Object lineaErorresObj = lineaErrores;
+                	
+					//Ahora convertirlo a json y trabajarlo como tal.
+                	JSONObject jsonObj = JSONObject.fromObject(lineaErorresObj);
+                	JSONArray results = jsonObj.getJSONArray("items");
+                	
+                	for(int i=0;i<results.size();i++){
+                		System.out.println(results.getJSONObject(i).getJSONArray("data"));
+                		JSONArray as = results.getJSONObject(i).getJSONArray("data");
+                		
+                		List<String> list = new ArrayList<String>();
+                		for (int n=0; n<as.size(); n++) {
+                		    list.add( as.getString(n));
+                		}
+                		for(String x : list){
+                			System.out.println(x);
+                		}
+                		//Ahora sólo falta insertar las cadenas en la bd.
+                	}
+                	
+                	/*
+                    for (JSONObject result : results.getValuesAs(JSONObject.class)) {
+                        System.out.print(result.getJsonObject("from").getString("name"));
+                        System.out.print(": ");
+                        System.out.println(result.getString("message", ""));
+                        System.out.println("-----------");
+                    }*/
+                	
+                	
+                	//System.out.println(jsonObj.getJSONObject("data"));
                 }
             }
 		} catch (Exception ioe) {
