4d80787db1092f2740b76778a884fb5799859efd
enyamartinez <envamapa@gmail.com>
2017-04-05 19:37:03 -0500

    Faltó un js ¬¬

---
+++ src/main/webapp/content/editor/proyectos/js/guionesprueba.js
@@ -0,0 +1,47 @@
+var contextPath = "prisma";
+$(document)
+.ready(
+		function() {
+			window.scrollTo(0, 0);
+			contextPath = $("#rutaContexto").val();
+			$('table.tablaGestion').DataTable();					
+			var json = $("#jsonGuionesTabla").val();
+			if (json !== "") {
+				var parsedJson = JSON.parse(json);
+				$.each(parsedJson, function(i, item) {
+					var id = item.id;
+					document.getElementById("checkbox-" + id).checked = true;
+				});
+			}
+		});
+
+function agregarMensaje(mensaje) {
+	alert(mensaje);
+};
+
+function prepararEnvio() {
+	try {
+		tablaToJson("tablaGuionesPrueba");
+		return true;
+	} catch (err) {
+		alert("Ha ocurrido un error." + err);
+	}
+}
+
+function tablaToJson(idTable) {
+	var table = $("#" + idTable).dataTable();
+	var arregloGuiones = [];
+	var seleccionado;
+
+	for (var i = 0; i < table.fnSettings().fnRecordsTotal(); i++) {
+		console.log("Entra al for");
+		var id = table.fnGetData(i, 1);
+		seleccionado = document.getElementById("checkbox-" + id).checked;
+		console.log(seleccionado);
+		if (seleccionado == true) {
+			arregloGuiones.push(new CasoUso(id));
+		}
+	}
+	var jsonGuiones = JSON.stringify(arregloGuiones);
+	document.getElementById("jsonGuionesTabla").value = jsonGuiones;
+}
